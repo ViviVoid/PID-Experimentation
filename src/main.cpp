a16f38d657c83eaf5e2b562d6b3de61c3755171e
Andy Dao <andydao2020@gmail.com>
2022-10-20 19:51:32 -0500

    Setting PID Experimentation

---
+++ src/main.cpp
@@ -0,0 +1,102 @@
+#include "main.h"
+#include "PID.h"
+
+/**
+ * Runs initialization code. This occurs as soon as the program is started.
+ *
+ * All other competition modes are blocked by initialize; it is recommended
+ * to keep execution time for this mode under a few seconds.
+ */
+void initialize() {
+	pros::lcd::initialize();
+	pros::lcd::set_text(1, "Hello PROS User!");
+}
+
+/**
+ * Runs while the robot is in the disabled state of Field Management System or
+ * the VEX Competition Switch, following either autonomous or opcontrol. When
+ * the robot is enabled, this task will exit.
+ */
+void disabled() {}
+
+/**
+ * Runs after initialize(), and before autonomous when connected to the Field
+ * Management System or the VEX Competition Switch. This is intended for
+ * competition-specific initialization routines, such as an autonomous selector
+ * on the LCD.
+ *
+ * This task will exit when the robot is enabled and autonomous or opcontrol
+ * starts.
+ */
+void competition_initialize() {}
+
+/**
+ * Runs the user autonomous code. This function will be started in its own task
+ * with the default priority and stack size whenever the robot is enabled via
+ * the Field Management System or the VEX Competition Switch in the autonomous
+ * mode. Alternatively, this function may be called in initialize or opcontrol
+ * for non-competition testing purposes.
+ *
+ * If the robot is disabled or communications is lost, the autonomous task
+ * will be stopped. Re-enabling the robot will restart the task, not re-start it
+ * from where it left off.
+ */
+void autonomous() {}
+
+/**
+ * Runs the operator control code. This function will be started in its own task
+ * with the default priority and stack size whenever the robot is enabled via
+ * the Field Management System or the VEX Competition Switch in the operator
+ * control mode.
+ *
+ * If no competition control is connected, this function will run immediately
+ * following initialize().
+ *
+ * If the robot is disabled or communications is lost, the
+ * operator control task will be stopped. Re-enabling the robot will restart the
+ * task, not resume it from where it left off.
+ */
+void opcontrol() {
+	pros::Controller master(pros::E_CONTROLLER_MASTER);
+	/*
+	-DECLARE MOTORS FOR PIDLIFTUSAGE
+	-DECLARE POTENTIOMETER
+	-USE POTENTIOMETER FOR CALCULATE.
+	-ERROR IS POTENTIOMETER ANGLE WANTED vs ANGLE AT.
+	*/
+	pros::Motor leftLiftMotor(18, false);
+	pros::Motor rightLiftMotor(17, true);
+	pros::ADIAnalogIn liftPotentiometer('H'); //0-4095 range, 250 degree constraint
+	pros::Motor_Group liftMotorGroup{leftLiftMotor, rightLiftMotor};
+
+	/*
+	pidLiftUsage : PID Tuning and calculating motor power based off error.
+	pidInUse : Disable liftmotor usage while pidInUse
+	desiredValue : desired angle.
+	*/
+	PID pidLiftUsage(0, 0, 0, 0);
+	bool pidInUse = false;
+	float desiredValue = 2000; //Desired value for PID
+	while (true) {
+		master.print(1,0,"Lift Raw Value: %d", liftPotentiometer.get_value());
+		if (pidInUse) //Lift controls
+		{
+			if (master.get_digital(pros::E_CONTROLLER_DIGITAL_L1))
+			{
+				liftMotorGroup.move(127);
+			}
+			if (master.get_digital(pros::E_CONTROLLER_DIGITAL_L2))
+			{
+				liftMotorGroup.move(-127);
+			}
+		}
+		else //Activates PID looping
+		{
+			liftMotorGroup.move(pidLiftUsage.calculate(desiredValue-liftPotentiometer.get_value())); //Move liftMotorGroup based off calculation from PID loop
+		}
+		if (master.get_digital(pros::E_CONTROLLER_DIGITAL_A)) //Controls pidInUse to determine if PID system is active or not
+		{
+			pidInUse = !pidInUse;
+		}
+	}
+}
